ec9eb746a625878cd78ae4fcd332c2bd78993c91
Gabriel <gabriellf.dev@gmail.com>
2022-07-12 18:20:46 -0300

    develop--bugfix--importUserModal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
 import { loginFunction } from "./loginFunction.scripts.js";
-import { modalUser } from "../controller/modalUser.scripts.js";
+import { UserModal } from "../controller/modalUser.controller.js";
 import { Modais } from "../controller/Modals.controller.js";
 loginFunction();
