ad0fc96fad264fa83f9dd026802176b724fd0221
Gabriel <gabriellf.dev@gmail.com>
2022-07-12 18:13:37 -0300

    develop--scriptindex

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
 import { loginFunction } from "./loginFunction.scripts.js";
-import { modalUser } from "../controllermodalUser.scripts.js";
+import { modalUser } from "../controller/modalUser.scripts.js";
 import { Modais } from "../controller/Modals.controller.js";
 loginFunction();
