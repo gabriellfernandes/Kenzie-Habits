0f290b35f48d955728b4a23260fe9bfd0b23fb0d
Gabriel <gabriellf.dev@gmail.com>
2022-07-13 15:16:08 -0300

    especial

--- src/controller/modalUser.controller.js
+++ src/controller/modalUser.controller.js
@@ -18,17 +18,8 @@ export class UserModal {
 
     static itemHabit(habit){
         this.baseTable.innerHTML = '';
-        this.baseTable.innerHTML = `
-        <tr id="table__header">
-            <section id="table__section">
-            <h3 id="table__status">Status</h3>
-            <h3 id="table__title">Titulo</h3>
-            <h3 id="table__description">Descrição</h3>
-            <h3 id="table__category">Categoria</h3>
-            <h3 id="table__edit">Editar</h3>
-            </section>
-        </tr>`
-        
+       
+
         habit.forEach(element => {
             const table__content = document.createElement('tr');
                 table__content.id = "table__content";
