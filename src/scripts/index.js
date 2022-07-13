f4c25f908ee112c607023899401eb35cb890daaf
Marchi8 <luandeoliveirasoares@gmail.com>
2022-07-13 09:08:09 -0300

    Main-Menu and new Features in development

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,19 @@
 import { loginFunction } from "./loginFunction.scripts.js";
 loginFunction();
+
+
+const checkbox = document.querySelector("#table__checkbox")
+checkbox.addEventListener("click", check)
+
+function check() {
+    if (checkbox.checked == true) {
+        const table__content = document.getElementById("table__content")
+        table__content.style.backgroundColor = "#F1F3F5"
+        table__content.style.textDecoration = "line-through"
+    }
+    if (checkbox.checked == false) {
+        const table__content = document.getElementById("table__content")
+        table__content.style.backgroundColor = "#FFFFFF"
+        table__content.style.textDecoration = "none"
+    }
+}
