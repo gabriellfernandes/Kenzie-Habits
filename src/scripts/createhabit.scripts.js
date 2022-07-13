f27d1b230925e55ca38de370ca97d14f90054576
Gabriel <gabriellf.dev@gmail.com>
2022-07-13 10:36:06 -0300

    commit--initfunctioncreatehabit

---
+++ src/scripts/createhabit.scripts.js
@@ -0,0 +1,8 @@
+import { Modais } from "../controller/Modals.controller";
+
+export function creatHabit(){
+    const habits__create = document.querySelector("#habits__create");
+    habits__create.addEventListener("click", (event) => {
+
+    })
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import { loginFunction } from "./loginFunction.scripts.js";
 import { UserModal } from "../controller/modalUser.controller.js";
 import { Modais } from "../controller/Modals.controller.js";
-console.log(localStorage.getItem('@CapStone-Token'))
+import { itemsTable } from "./itemtable.script.js";
+import { Logout } from "./logout.js";
+
 if(localStorage.getItem('@CapStone-Token') != null){
     const modalLogin = document.getElementById('login__modal');
     modalLogin.style.display = 'none';
@@ -9,16 +11,10 @@ if(localStorage.getItem('@CapStone-Token') != null){
     const modalLogin = document.getElementById('login__modal');
     modalLogin.style.display = 'flex';
 }
-
-import { Logout } from "./logout.js";
-
-
 loginFunction();
-
-
+itemsTable();
 const checkbox = document.querySelector("#table__checkbox")
 checkbox.addEventListener("click", check)
-
 function check() {
     if (checkbox.checked == true) {
         const table__content = document.getElementById("table__content")
@@ -31,7 +27,6 @@ function check() {
         table__content.style.textDecoration = "none"
     }
 }
-
 
 const logoutButton = document.getElementById("header__logout")
 logoutButton.addEventListener("click", Logout.logoutButton)
